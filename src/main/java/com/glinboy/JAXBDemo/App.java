c8057c77fdd044767e316a03e27ca5dff682f568
GLinBoy <hojjatabedie@gmail.com>
2015-12-11 18:31:28 +0330

    Add Marshaller & Unmarshaller test

--- src/main/java/com/glinboy/JAXBDemo/App.java
+++ src/main/java/com/glinboy/JAXBDemo/App.java
@@ -1,13 +1,37 @@
 package com.glinboy.JAXBDemo;
 
-/**
- * Hello world!
- *
- */
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.glinboy.patient.Patient;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+			JAXBContext context = JAXBContext.newInstance(Patient.class);
+			Marshaller marshaller = context.createMarshaller();
+			Patient patient = new Patient();
+			patient.setId(230);
+			patient.setName("GLinBoy");
+			StringWriter writer = new StringWriter();
+			marshaller.marshal(patient, writer);
+			System.out.println(writer.toString());
+			
+			Unmarshaller unmurshaller = context.createUnmarshaller();
+			Patient patientResult = (Patient) unmurshaller.unmarshal(new StringReader(writer.toString()));
+			
+			System.out.println("---+> Name: " + patient.getName());
+			
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
